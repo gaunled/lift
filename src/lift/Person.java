f97147c311e811f04b0b80b341a0f01bc390f4d4
Daniel Gaunled <gaunled@gmail.com>
2017-04-19 17:55:07 +0700

    ini nama commit nya

--- src/lift/Person.java
+++ src/lift/Person.java
@@ -5,16 +5,27 @@
  */
 package lift;
 
-/**
- *
- * @author Daniel
- */
+import java.util.Random;
+
 public class Person {
     String id;
     String imageurl;
     int awal;
     int akhir;
+    int x;
     boolean finish;
+
+    public Person(String id, int jmllantai) {
+        Random rand = new Random();
+        this.id = id;
+        this.imageurl = "img/Standing Man Filled-50.png";
+        this.awal = rand.nextInt(jmllantai);
+        do {
+            this.akhir = rand.nextInt(jmllantai);
+        } while (this.awal == this.akhir);
+        this.finish = false;
+        this.x = 20;
+    }
     
     public String getId() {
         return id;
@@ -48,6 +59,14 @@ public class Person {
         this.akhir = akhir;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+    
     public boolean isFinish() {
         return finish;
     }
